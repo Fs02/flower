e43602f25ab51a94354b062fc2e9344d1d4aede5
Fs02 <surya.asriadie@gmail.com>
2016-10-04 01:39:11 +0700

    some refactoring and make convolve function to support batch input

--- include/flower/layer/convolution.h
+++ include/flower/layer/convolution.h
@@ -2,28 +2,35 @@
 #define FLOWER_CONVOLUTION_H
 
 #include <flower/layer.h>
+#include <vector>
 
 namespace flower
 {
-    class ConvolutionDef : public ILayerDef
+    class Convolution : public ILayerDef
     {
     public:
-        ConvolutionDef();
+        Convolution(const Eigen::array<int, 3>& filter_dims, int filter_num, int stride, int padding);
 
         inline const char *type() const { return "Tanh"; }
+
+        inline const Eigen::array<int, 3> &filter_dims() const { return filter_dims_; }
+        inline int filter_num() const { return filter_num_; }
+        inline int stride() const { return stride_; }
+        inline int padding() const { return padding_; }
 
     protected:
         layer_ptr create(Net *net) const;
 
-        int filter_;
+        Eigen::array<int, 3> filter_dims_;
+        int filter_num_;
         int stride_;
         int padding_;
     };
 
-    class Convolution : public ILayer
+    class ConvolutionLayer : public ILayer
     {
     public:
-        explicit Convolution(Net *net, const ConvolutionDef &definition);
+        explicit ConvolutionLayer(Net *net, const Convolution &definition);
 
         inline const char *type() const { return "Tanh"; }
 
@@ -33,10 +40,11 @@ namespace flower
         Eigen::Tensor<double, 4> forward(const Eigen::Tensor<double, 4> &data, bool train = false);
         Eigen::Tensor<double, 4> backward(const Eigen::Tensor<double, 4> &errors);
 
-        static void convolve(const Eigen::Tensor<double, 3>& input, const Eigen::Tensor<double, 3>& filter, double bias, Eigen::Tensor<double, 2>& output, int stride);
+        static void convolve(const Eigen::Tensor<double, 4>& input, const Eigen::Tensor<double, 3>& filter, double bias, Eigen::Tensor<double, 3>& output, int stride);
 
     protected:
-        Eigen::Tensor<double, 3> kernel_;
+        std::vector<Eigen::Tensor<double, 3>> filters_;
+        std::vector<double> biases_;
         int stride_;
         int padding_;
     };
--- src/flower/layer/convolution.cpp
+++ src/flower/layer/convolution.cpp
@@ -2,53 +2,65 @@
 
 using namespace flower;
 
-inline double d_tanh(double x) { return 1.0 - pow(tanh(x), 2.0); }
-
-ConvolutionDef::ConvolutionDef()
-    : ILayerDef()
+Convolution::Convolution(const Eigen::array<int, 3>& filter_dims, int filter_num, int stride, int padding)
+    : ILayerDef(), filter_dims_(filter_dims), filter_num_(filter_num), stride_(stride), padding_(padding)
 {}
 
-layer_ptr ConvolutionDef::create(Net *net) const
+layer_ptr Convolution::create(Net *net) const
 {
-    return std::make_shared<Convolution>(net, *this);
+    return std::make_shared<ConvolutionLayer>(net, *this);
 }
 
 
-Convolution::Convolution(Net *net, const ConvolutionDef &definition)
-    : ILayer(net, definition)
-{}
+ConvolutionLayer::ConvolutionLayer(Net *net, const Convolution &definition)
+    : ILayer(net, definition), filters_(definition.filter_num()), biases_(definition.filter_num()),
+      stride_(definition.stride()), padding_(definition.padding())
+{
+    // initialize random filters
+    Eigen::Tensor<double, 3> filter(definition.filter_dims()[0], definition.filter_dims()[1], definition.filter_dims()[2]);
+    for (int i = 0; i < definition.filter_num(); ++i)
+    {
+        filters_.push_back(filter.random());
+        biases_.push_back(0.0);
+    }
+}
 
-Eigen::Tensor<double, 2> Convolution::forward(const Eigen::Tensor<double, 2> &data, bool train)
+Eigen::Tensor<double, 2> ConvolutionLayer::forward(const Eigen::Tensor<double, 2> &data, bool train)
 {
     return data;
 }
 
-Eigen::Tensor<double, 2> Convolution::backward(const Eigen::Tensor<double, 2> &errors)
+Eigen::Tensor<double, 2> ConvolutionLayer::backward(const Eigen::Tensor<double, 2> &errors)
 {
     return errors;
 }
 
-Eigen::Tensor<double, 4> Convolution::forward(const Eigen::Tensor<double, 4> &data, bool train)
+Eigen::Tensor<double, 4> ConvolutionLayer::forward(const Eigen::Tensor<double, 4> &data, bool train)
 {
 
 }
 
-Eigen::Tensor<double, 4> Convolution::backward(const Eigen::Tensor<double, 4> &errors)
+Eigen::Tensor<double, 4> ConvolutionLayer::backward(const Eigen::Tensor<double, 4> &errors)
 {
 
 }
 
-void Convolution::convolve(const Eigen::Tensor<double, 3>& input, const Eigen::Tensor<double, 3>& filter, double bias, Eigen::Tensor<double, 2>& output, int stride)
+void ConvolutionLayer::convolve(const Eigen::Tensor<double, 4>& input, const Eigen::Tensor<double, 3>& filter, double bias, Eigen::Tensor<double, 3>& output, int stride)
 {
-    for (int i = 0; i <= input.dimension(1) - filter.dimension(1); i += stride)
+    // TODO: replace with eigen built in convolve
+    for (int i = 0; i < input.dimension(0); ++i)
     {
-        for (int j = 0; j <= input.dimension(2) - filter.dimension(2); j += stride)
+        for (int j = 0; j <= input.dimension(2) - filter.dimension(1); j += stride)
         {
-            // perform dot product between local region and kernel
-            Eigen::array<long int, 3> offsets = {0, i, j};
-            Eigen::array<long int, 3> extents = {3, filter.dimension(1), filter.dimension(2)};
-            Eigen::Tensor<double, 0> dot_product = (input.slice(offsets, extents) * filter).sum();
-            output(i/stride, j/stride) = dot_product(0) + bias;
+            for (int k = 0; k <= input.dimension(3) - filter.dimension(2); k += stride)
+            {
+                // perform dot product between local region and kernel
+                Eigen::array<long int, 4> offsets = {i, 0, j, k};
+                Eigen::array<long int, 4> extents = {1, 3, filter.dimension(1), filter.dimension(2)};
+                Eigen::Tensor<double, 3> local = input.slice(offsets, extents).reshape(filter.dimensions());
+                Eigen::Tensor<double, 0> dot_product = (local * filter).sum();
+                output(i, j/stride, k/stride) = dot_product(0) + bias;
+            }
         }
     }
 }
